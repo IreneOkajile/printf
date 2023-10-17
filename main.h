808e5c91c2aa164b9202c1788f3b9715e1bc6649
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-10-17 12:17:46 -0700

    alx/Holberton printf project

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdarg.h>
+#include<string.h>
+#include<unistd.h>
+int printf_str(char *);
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+
+#endif
---
+++ myprintf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _printf - mimic standard printf
+ * @format: pointer
+ * Return: integer
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i, count = 0;
+	va_list list;
+
+	if (!format)
+		return (-1);
+	va_start(list, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+		}
+		else if (format[i + 1] == 'c')
+		{
+			_putchar(va_arg(list, int));
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			int s = printf_str(va_arg(list, char *));
+			i++;
+			count += (s - 1);
+		}
+		else if (format[i + 1] == '%')
+		{
+			_putchar('%');
+			i++;
+		}
+	}
+	_putchar('\n');
+	return (count);
+
+}
