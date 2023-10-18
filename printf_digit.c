59fed0ef0ae18ef396dfb1c7a067c2ebc8de9a7b
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-10-18 08:05:51 -0700

    alx/Holberton c-printf projet

--- printf_digit.c
+++ printf_digit.c
@@ -5,80 +5,16 @@
  * @n: number to print
  * Return: return integer
  */
-int printf_digit_d(int n)
+int printf_digit_d(int num)
 {
-	int num, last = n % 10, digit, exp = 1, count = 1;
+	int count = 0, n;
 
-	n = n / 10;
-	num = n
+	if (num == 0)
+		return (0);
 
-	if (last < 0)
-	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		count++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			count++;
-		}
-	}
-	_putchar(last + '0');
-	return (count);
-}
-
-/**
- * printf_digit_i - print an int
- * @n: number to be printed
- * Return: integer
- */
-
-int printf_digit_i(int n)
-{
-	int num, last = n % 10, digit, exp = 1, count = 1;
-
-	n = n / 10;
-	num = n
-
-	if (last < 0)
-	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		count++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			count++;
-		}
-	}
-	_putchar(last + '0');
+	n = num / 10;
+	printf_digit_d(n);
+	_putchar(num % 10 + '0');
+	count += 1;
 	return (count);
 }
